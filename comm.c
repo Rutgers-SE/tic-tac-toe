e4d3b3b6ba1074feeb479a22ce0e4fa90aa561c6
Kerry Liu <kerryliu123@gmail.com>
2017-04-29 14:40:52 -0400

    whoops

--- comm.c
+++ comm.c
@@ -97,7 +97,7 @@ void board_print_from_string(char *board_string) {
   int board[3][3];
   sscanf(board_string, "%d-%d-%d-%d-%d-%d-%d-%d-%d", 
          &board[0][0], &board[0][1], &board[0][2], 
-         &board[0][1], &board[1][1], &board[1][2], 
+         &board[1][0], &board[1][1], &board[1][2], 
          &board[2][0], &board[2][1], &board[2][2]);
   print_board(board);
 }
